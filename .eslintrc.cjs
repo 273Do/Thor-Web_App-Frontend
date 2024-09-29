01f9dedbee81d36c3faf298ec41ef28f1fcf18c9
273* <keita.y310@icloud.com>
2024-09-30 00:46:41 +0900

    フォーマッタを導入しました．

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -34,7 +34,74 @@ module.exports = {
         "plugin:react/jsx-runtime",
         "plugin:react-hooks/recommended",
         "plugin:jsx-a11y/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:tailwindcss/recommended",
       ],
+      rules: {
+        "@typescript-eslint/consistent-type-imports": [
+          "error",
+          {
+            prefer: "type-imports",
+            fixStyle: "separate-type-imports",
+          },
+        ],
+        "@typescript-eslint/no-unused-vars": [
+          "warn",
+          {
+            vars: "all",
+            varsIgnorePattern: "^_",
+            args: "after-used",
+            argsIgnorePattern: "^_",
+          },
+        ],
+        "object-shorthand": "error",
+        "tailwindcss/classnames-order": "warn",
+        "tailwindcss/enforces-negative-arbitrary-values": "warn",
+        "tailwindcss/enforces-shorthand": "warn",
+        "tailwindcss/migration-from-tailwind-2": "warn",
+        "tailwindcss/no-arbitrary-value": "off",
+        "tailwindcss/no-contradicting-classname": "error",
+        "tailwindcss/no-custom-classname": "off",
+        "tailwindcss/no-unnecessary-arbitrary-value": "warn",
+
+        "react/jsx-curly-brace-presence": "error",
+        "react/self-closing-comp": [
+          "error",
+          {
+            component: true,
+            html: false,
+          },
+        ],
+        "import/order": [
+          "error",
+          {
+            groups: [
+              "builtin",
+              "external",
+              "internal",
+              "parent",
+              "sibling",
+              "index",
+            ],
+            "newlines-between": "always",
+            alphabetize: { order: "asc", caseInsensitive: false },
+            pathGroups: [
+              {
+                pattern: "react**",
+                group: "external",
+                position: "before",
+              },
+              {
+                pattern: "@material-ui/**",
+                group: "external",
+                position: "after",
+              },
+            ],
+            pathGroupsExcludedImportTypes: ["react"],
+          },
+        ],
+        "unused-imports/no-unused-imports": "error",
+      },
       settings: {
         react: {
           version: "detect",
@@ -53,7 +120,7 @@ module.exports = {
     // Typescript
     {
       files: ["**/*.{ts,tsx}"],
-      plugins: ["@typescript-eslint", "import"],
+      plugins: ["@typescript-eslint", "import", "unused-imports"],
       parser: "@typescript-eslint/parser",
       settings: {
         "import/internal-regex": "^~/",
